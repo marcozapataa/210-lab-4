749d0fec68da41de6e66d42849bd310edc704660
Marco <mazapata053@gmail.com>
2026-09-05 08:02:59 -0700

    Milestone 1: struct works and can recieve initialization data, outputs the data to the console.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,32 @@
 
 using namespace std;
 
+struct Color
+{
+    int red;
+    int green;
+    int blue;
+};
+
 int main()
 {
+    // initialize the Color object
+    Color myColor;
 
+    // prompt user to enter data
+    cout << "Enter red value: " << endl;
+    cin >> myColor.red;
+
+    cout << "Enter green value: " << endl;
+    cin >> myColor.green;
+
+    cout << "Enter blue value: " << endl;
+    cin >> myColor.blue;
+
+    // output the object's data to the console to confirm
+    cout << "R value: " << myColor.red << endl;
+    cout << "G value: " << myColor.green << endl;
+    cout << "B value: " << myColor.blue << endl;
 
     return 0;
 }
