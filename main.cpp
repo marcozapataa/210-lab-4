9873babfad5b98b9a4627277efbb8ed96c8a0b2a
Marco <mazapata053@gmail.com>
2026-09-05 08:22:22 -0700

    Milestone 2: vector has been tested and it outputs to the console.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -28,6 +29,16 @@ int main()
     cout << "R value: " << myColor.red << endl;
     cout << "G value: " << myColor.green << endl;
     cout << "B value: " << myColor.blue << endl;
+
+    // create an empty vector of type <Color>
+     vector <Color> myVec;
+
+     // populate the vector with the Color object
+     myVec.push_back(myColor);
+
+     cout << "Stored R: " << myVec[0].red << endl;
+     cout << "Stored G: " << myVec[0].green << endl;
+     cout << "Stored B: " << myVec[0].blue << endl;
 
     return 0;
 }
